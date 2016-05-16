da282e3e4ff86009070e827d6996f72d009e3558
Eder Rodrigues <Eder Rodrigues@Home>
2016-05-15 21:31:14 -0300

    Carrinho Funcionando + Logica finalizar venda (incompleto)

--- src/main/java/com/senac/sportnet/web/managedbean/CompraBean.java
+++ src/main/java/com/senac/sportnet/web/managedbean/CompraBean.java
@@ -8,6 +8,7 @@ package com.senac.sportnet.web.managedbean;
 import com.senac.spornet.entity.Produto;
 import com.senac.sportnet.fakeimpl.ProdutoServiceFakeImpl;
 import com.senac.sportnet.service.ProdutoService;
+import com.senac.sportnet.servicejpa.ProdutoServiceJPA;
 import com.senac.sportnet.web.entity.ProdutoQuantidade;
 import com.senac.sportnet.web.util.Mensagem;
 import java.io.Serializable;
@@ -25,65 +26,67 @@ import javax.faces.context.Flash;
  *
  * @author nliggia-ibm
  */
-
 @ManagedBean
 @SessionScoped
 public class CompraBean implements Serializable {
-  
-  private Set<ProdutoQuantidade> itens = 
-          new LinkedHashSet<ProdutoQuantidade>();
-  
-  private ProdutoQuantidade obterItem(Produto produto) {
-    for (ProdutoQuantidade pq : itens) {
-      if (pq.getProduto().equals(produto)) {
-        return pq;
-      }
-    }
-    return null;
-  }
-  
-  public String adicionarProduto(long idProduto, int quantidade) {
-    // obter objeto produto a partir do id
-    ProdutoService prodService = new ProdutoServiceFakeImpl();
-    Produto p = prodService.obter(idProduto);
-    
-    ProdutoQuantidade pq = obterItem(p);
-    if (pq == null) {
-      // Cria um novo item para o produto e quantidade informados
-      itens.add(new ProdutoQuantidade(p, quantidade));
-    } else {
-      // Altera a quantidade informada do produto
-      pq.setQuantidade(quantidade);
-    }
-    
-    // Mensagem de sucesso para usuário
-    Flash flash = FacesContext.getCurrentInstance()
-            .getExternalContext().getFlash();
-    flash.put("mensagem", new Mensagem("Produto '" 
-            + p.getNome() 
-            + "' adicionado com sucesso", "success"));
-    
-    // Redireciona para tela de listagem de produtos
-    return "listaTenis.xhtml?faces-redirect=true";
-  }
-  
-  // Converte Set em List senao nao funciona no ui:repeat
-  public List<ProdutoQuantidade> getItens() {
-    List<ProdutoQuantidade> lista = 
-            new ArrayList<ProdutoQuantidade>();
-    lista.addAll(itens);
-    return lista;
-  }
-  
-  
-//  ARRUMAR METODO QUE PEGA VALOR TOTAL, DO TIPO FLOAT
-//  public BigDecimal getValorTotal() {
-//    BigDecimal total = new BigDecimal(0);
-//    for (ProdutoQuantidade pq : itens) {
-//      total = total.add(pq.getPreco());
-//    }
-//    return total;
-//  }
-  
-}
 
+    private Set<ProdutoQuantidade> itens
+            = new LinkedHashSet<ProdutoQuantidade>();
+
+    private ProdutoQuantidade obterItem(Produto produto) {
+        for (ProdutoQuantidade pq : itens) {
+            if (pq.getProduto().equals(produto)) {
+                return pq;
+            }
+        }
+        return null;
+    }
+
+    public String adicionarProduto(long idProduto, int quantidade) {
+        // obter objeto produto a partir do id
+        ProdutoService prodService = new ProdutoServiceJPA();
+        Produto p = prodService.obter(idProduto);
+
+        ProdutoQuantidade pq = obterItem(p);
+        if (pq == null) {
+            // Cria um novo item para o produto e quantidade informados
+            itens.add(new ProdutoQuantidade(p, quantidade));
+        } else {
+            // Altera a quantidade informada do produto
+            pq.setQuantidade(quantidade);
+        }
+
+        // Mensagem de sucesso para usuário
+        Flash flash = FacesContext.getCurrentInstance()
+                .getExternalContext().getFlash();
+        flash.put("mensagem", new Mensagem("Produto '"
+                + p.getNome()
+                + "' adicionado com sucesso", "success"));
+
+        // Redireciona para tela de listagem de produtos
+        return "listaTenis.xhtml?faces-redirect=true";
+    }
+
+    // Converte Set em List senao nao funciona no ui:repeat
+    public List<ProdutoQuantidade> getItens() {
+        List<ProdutoQuantidade> lista
+                = new ArrayList<ProdutoQuantidade>();
+        lista.addAll(itens);
+        return lista;
+    }
+
+    //ARRUMAR METODO QUE PEGA VALOR TOTAL, DO TIPO FLOAT
+    public float getValorTotal() {
+        float total = 0;
+        for (ProdutoQuantidade pq : itens) {
+            total += pq.getPreco();
+        }
+        return total;
+    }
+
+    public void fecharCompra() {
+        ProdutoService prodService = new ProdutoServiceJPA();
+        
+    }
+
+}
