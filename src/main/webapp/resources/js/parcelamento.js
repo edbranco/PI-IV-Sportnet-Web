59ced67e25b71fe8ddd233ff142e129602908c58
DU <DU@DU-PC.home>
2016-06-11 00:27:53 -0300

    Ajustes finais no front

--- src/main/webapp/resources/js/parcelamento.js
+++ src/main/webapp/resources/js/parcelamento.js
@@ -3,5 +3,6 @@ $(function () {
     parcelamento = $(".detalhesProduto").text().replace("R$","");
     parcelamento = parseFloat(parcelamento);
     parcelamento /= 4;
+    parcelamento = parcelamento.toFixed(2);
     $(".detalhesProduto p:nth-of-type(2)").html("4x de R$ "+parcelamento+" sem juros");
 });
