8bcf30c3d5103fc77b2bf226906719bfa974129d
nliggia-ibm <nliggia@br.ibm.com>
2016-06-14 18:25:07 -0300

    ajustes layout relatorioAll

--- src/main/java/com/senac/sportnet/web/managedbean/AdminBean.java
+++ src/main/java/com/senac/sportnet/web/managedbean/AdminBean.java
@@ -85,7 +85,7 @@ public class AdminBean implements Serializable {
         flash.put("mensagem", new Mensagem("Admin '"
                 + c.getNome()
                 + "' cadastrado com sucesso", "success"));
-        return "adminCadastrado.xhtml?faces-redirect=true";
+        return "admin-menu.xhtml?faces-redirect=true";
 
     }
 
